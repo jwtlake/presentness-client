0f0f0666923e8a275c09ebdf50e9b9acb2703af7
jwtlake <jwtlake@gmail.com>
2015-10-15 07:32:49 -0700

    minor change

--- src/server/models/webLogger.js
+++ src/server/models/webLogger.js
@@ -9,21 +9,23 @@ module.exports = {
 	sendUpdate: function(update){
 		//get server info
 		var server = settings.get('server:ip') + ':' + settings.get('server:port');
-		var postURL = 'http://'+ server +'/api/device/'+ alias;
 		var postData = update;
 
 		var alias = update.alias; //need to come up with a primary key. Would like to use mac but : char might cause problems
+		var cleanMac = update.macAddress.replace(/:/g,'');
 		// var macAddress = update.macAddress;
 		// var ip  = update.ip;
 		// var isPresent = update.isPresent;
 		// var lastSeen = update.lastSeen;
 
 		console.log('Sending Update for: ' + alias);
+		console.log('CleanMac: ' + cleanMac);
 		console.log('Data: ' + postData);
+		console.log(postData);
 
 		//send update to server
 		request.post({
-			url: postURL,
+			url: 'http://'+ server +'/api/device/'+ cleanMac,
 			form: postData
 		}, function(err, httpResponse, body) {
 			if(err){
